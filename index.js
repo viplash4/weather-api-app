76f86160d8aae6ea1da9ec33ecdbfee8537c05af
viplash4 <maxzilvova@gmail.com>
2023-03-29 18:45:53 +0300

    todo: display promise and find surise sunset

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hello = void 0;
-const world = 'world';
-function hello(who = world) {
-    return `Hello ${who}! `;
-}
-exports.hello = hello;
+var apiUrl = "https://api.sunrise-sunset.org/json";
+var date = new Date();
+var lat = 37.7749;
+var lng = -122.4194;
+var url = "".concat(apiUrl, "?lat=").concat(lat, "&lng=").concat(lng, "&date=").concat(date.toISOString().slice(0, 10));
+fetch(url)
+    .then(function (response) { return response.json(); })
+    .then(function (data) {
+    var _a = data.results, sunrise = _a.sunrise, sunset = _a.sunset;
+    console.log("Sunrise at ".concat(sunrise, ", Sunset at ").concat(sunset));
+})["catch"](function (error) { return console.error(error); });
