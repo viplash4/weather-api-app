8f2c15a98c500399a286ab79a55be4a441315749
viplash4 <maxzilvova@gmail.com>
2023-04-06 11:26:14 +0300

    getMinSunset added, new post request

--- src/getTaskSunset.ts
+++ src/getTaskSunset.ts
@@ -61,62 +61,19 @@ export const getMinSunrise = (sunriseSunsetData: SunriseSunsetData[]) => {
     
     return acc;
   }, initialData);
-  return [minSunrise.sunrise, minSunrise.day_length];
-
-  //return [sortedData[0].sunrise, sortedData[0].day_length];
+  return [minSunrise.sunrise, minSunrise.day_length]; 
 }
-/*
-getMinSunrise working version ->
+export const getMaxSunrise = (sunriseSunsetData: SunriseSunsetData[]) => {
 
-const sortedData = sunriseSunsetData.sort((a, b) => {
-    const aSunrise = new Date(`1970-01-01T${a.sunrise}`);
-    const bSunrise = new Date(`1970-01-01T${b.sunrise}`);
-    return aSunrise.getTime() - bSunrise.getTime();
-  });
-  return sortedData[0].sunrise;
+  const initialData = sunriseSunsetData[0];
+  const maxSunrise = sunriseSunsetData.reduce((acc, curr) => {
+    if (curr.sunrise > acc.sunrise && curr.day_length !== 0) {
+      
+      return curr;
+    } 
+    
+    return acc;
+  }, initialData);
+  return [maxSunrise.sunrise, maxSunrise.day_length]; 
+}
 
-
-
-
-export const convertSunriseToTime = (sunrise: string): Date | null => {
-    const sunriseParts = sunrise.split(":");
-    if (sunriseParts.length !== 3) {
-      return null;
-    }
-  
-    const hours = parseInt(sunriseParts[0]);
-    const minutes = parseInt(sunriseParts[1]);
-    const seconds = parseInt(sunriseParts[2]);
-  
-    if (isNaN(hours) || isNaN(minutes) || isNaN(seconds)) {
-      return null;
-    }
-  
-    const today = new Date();
-    const sunriseDate = new Date(
-      today.getFullYear(),
-      today.getMonth(),
-      today.getDate(),
-      hours,
-      minutes,
-      seconds
-    );
-    return sunriseDate;
-};
-
-export const getMinSunrise = (results: SunriseSunsetData[]): Date | null => {
-    let minSunrise: Date | null = null;
-    for (const result of results) {
-      const sunrise = result.sunrise;
-      if (sunrise != null) {
-        const sunriseDate = convertSunriseToTime(sunrise);
-        if (sunriseDate != null) {
-          if (minSunrise == null || sunriseDate < minSunrise) {
-            minSunrise = sunriseDate;
-          }
-        }
-      }
-    }
-    return minSunrise;
-};
-*/
