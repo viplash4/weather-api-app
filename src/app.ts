1d181989d2809d28baccf75532cd99fbaf02ce66
viplash4 <maxzilvova@gmail.com>
2023-04-12 20:01:04 +0300

    squelize troubles

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,10 @@ import { ErrorHandler } from './middlewares/ErrorHandler';
 import * as express from 'express';
 
 import { index } from './routes/index';
-
+import * as bodyparser from 'body-parser';
 export const app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.use('/', index);
+app.use(bodyparser.json());
 app.use(ErrorHandler);
