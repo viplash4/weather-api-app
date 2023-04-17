b8fad981bb61c08d87b465cb6a498e762d39cac4
Mykhailo <miansu01@gmail.com>
2023-04-17 19:37:32 +0100

    remove test version

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,13 +1,10 @@
 import { Sequelize } from 'sequelize';
-import environment from './environment';
-import defineUser from '../models/User';
 
-export const initDatabase = async () => {
-    const bd = new Sequelize({
-        dialect: 'postgres',
-        ...environment.database,
-    });
-
-    await defineUser(bd);
-    return bd;
-};
+export const database = new Sequelize({
+    dialect: 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    port: Number(process.env.DB_PORT) || 5432,
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
+    database: process.env.DB_NAME || 'postgres',
+});
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,22 +1,20 @@
-import { DataTypes, Sequelize } from 'sequelize';
+import { DataTypes } from 'sequelize';
+import { database } from '../config/database';
 
-const defineUser = async (database: Sequelize) => {
-    const User = database.define('User', {
-        name: {
-            type: DataTypes.STRING,
-        },
-        password: {
-            type: DataTypes.STRING,
-        },
-        email: {
-            type: DataTypes.STRING,
-        },
-        birthDate: {
-            type: DataTypes.DATEONLY,
-        },
-    });
+const User = database.define('User', {
+    name: {
+        type: DataTypes.STRING,
+    },
+    password: {
+        type: DataTypes.STRING,
+    },
+    email: {
+        type: DataTypes.STRING,
+    },
+    birthDate: {
+        type: DataTypes.DATEONLY,
+    },
+});
 
-    await User.sync();
-    return User;
-};
-export default defineUser;
+User.sync();
+export default User;
