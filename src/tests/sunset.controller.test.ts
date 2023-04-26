9021984a4c7ebc1ea0fb07a3107e5c9b4e12301d
viplash4 <maxzilvova@gmail.com>
2023-04-26 07:53:15 +0300

    test more functions

--- src/tests/sunset.controller.test.ts
+++ src/tests/sunset.controller.test.ts
@@ -1,4 +1,9 @@
-import { fetchData } from '../controller/sunset.controller';
+import {
+    fetchData,
+    generateCoords,
+    getRandomCoords,
+    multiplePointsFetch,
+} from '../controller/sunset.controller';
 
 describe('fetchData', () => {
     it('should fetch data from sunrise-sunset API', async () => {
@@ -7,5 +12,36 @@ describe('fetchData', () => {
         expect(data.sunrise).toBeDefined();
         expect(data.sunset).toBeDefined();
         expect(data.day_length).toBeDefined();
+    });
+});
+describe('generateCoords', () => {
+    it('should return an array of the specified length', () => {
+        const length = 5;
+        const result = generateCoords(length);
+        expect(result.length).toBe(length);
+    });
+});
+describe('getRandomCoords', () => {
+    test('returns random coordinates within expected range', () => {
+        const result = getRandomCoords();
+        expect(result.latitude).toBeGreaterThanOrEqual(-90);
+        expect(result.latitude).toBeLessThanOrEqual(90);
+        expect(result.longitude).toBeGreaterThanOrEqual(-180);
+        expect(result.longitude).toBeLessThanOrEqual(180);
+    });
+});
+describe('multiplePointsFetch', () => {
+    test('multiplePointsFetch returns data for all provided coordinates', async () => {
+        const coords = [
+            { latitude: 40.7128, longitude: -74.006 },
+            { latitude: 51.5074, longitude: -0.1278 },
+            { latitude: 35.6895, longitude: 139.6917 },
+        ];
+        const results = await multiplePointsFetch(coords);
+        expect(results.length).toBe(coords.length);
+        results.forEach((result) => {
+            expect(result).toHaveProperty('sunrise');
+            expect(result).toHaveProperty('sunset');
+        });
     });
 });
